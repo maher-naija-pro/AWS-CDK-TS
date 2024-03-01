4f7aefe64b7006ec336e6e8fcb996b4b9f905bdf
mnstudios <maher.naija@gmail.com>
2024-03-01 15:53:36 +0000

    update

--- lib/aws-cdk-ts-stack.ts
+++ lib/aws-cdk-ts-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 
@@ -9,12 +9,6 @@ export class AwsCdkTsStack extends cdk.Stack {
     super(scope, id, props);
 
 
-const vpc = new ec2.Vpc(this, 'Vpc', {
-  ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
-});
 
-     const queue = new sqs.Queue(this, 'AwsCdkTsQueue', {
-       visibilityTimeout: cdk.Duration.seconds(300)
-     });
   }
 }
