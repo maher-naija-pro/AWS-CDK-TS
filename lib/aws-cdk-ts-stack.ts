efa034783b0f0af0c64953fe568acf2e64a7fb69
mnstudios <maher.naija@gmail.com>
2024-03-01 15:43:00 +0000

    update

--- lib/aws-cdk-ts-stack.ts
+++ lib/aws-cdk-ts-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
 
 export class AwsCdkTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+const vpc = new ec2.Vpc(this, 'Vpc', {
+  ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
+});
+
+     const queue = new sqs.Queue(this, 'AwsCdkTsQueue', {
+       visibilityTimeout: cdk.Duration.seconds(300)
+     });
   }
 }
